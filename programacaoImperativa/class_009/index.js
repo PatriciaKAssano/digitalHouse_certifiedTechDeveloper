83524b619a0dccfcb91d068c69cb55bc834f6219
PatriciaKAssano <pkassano@gmail.com>
2021-08-28 21:59:56 -0300

    Add project - programacaoImperativa/class_009

---
+++ programacaoImperativa/class_009/index.js
@@ -0,0 +1,64 @@
+// Tempo para construção do código: até 22:20
+// Tempo para teste e correções do código: até 22:40
+// Turma: 01 NT
+// Integrantes: Alisson Pinho, Hiego Willy, Jhonathan Weber, Patricia Kushima Assano, Yasmin Lorraine
+
+
+function rapidFood(opcao, tempo) {
+    let msg;
+  
+  if (opcao > 5) {
+    msg = ("Prato inexistente!");
+  } else if (opcao <= 5) {
+    if (opcao == 1) {
+      if (tempo > 20 && tempo <= 30) {
+        msg = ("A comida queimou!");
+      } else if (tempo < 10) {
+        msg = ("Tempo insuficiente!");
+      } else if (tempo > 30) {
+        msg = ("Kabumm!");
+      } else {
+        msg = ("Prato pronto, bom apetite!!!");
+      }
+    } else if (opcao == 2 || opcao == 5) {
+      if (tempo > 16 && tempo <= 24) {
+        msg = ("A comida queimou!");
+      } else if (tempo < 8) {
+        msg = ("Tempo insuficiente!");
+      } else if (tempo > 24) {
+        msg = ("Kabumm!");
+      } else {
+        msg = ("Prato pronto, bom apetite!!!");
+      }
+    } else if (opcao == 3) {
+      if (tempo > 30 && tempo <= 45) {
+        msg = ("A comida queimou!");
+      } else if (tempo < 15) {
+        msg = ("Tempo insuficiente!");
+      } else if (tempo > 45) {
+        msg = ("Kabumm!");
+      } else {
+        msg = ("Prato pronto, bom apetite!!!");
+      }
+    } else if (opcao == 4) {
+      if (tempo > 24 && tempo <= 36) {
+        msg = ("A comida queimou!");
+      } else if (tempo < 12) {
+        msg = ("Tempo insuficiente!");
+      } else if (tempo > 36) {
+        msg = ("Kabumm!");
+      } else {
+        msg = ("Prato pronto, bom apetite!!!");
+      }
+    }
+  }
+  return msg;
+ }
+  
+ /*console.log("1: Pipoca");
+ console.log("2: Macarrão");
+ console.log("3: Carne");
+ console.log("4: Feijão");
+ console.log("5: Brigadeiro");*/
+  
+ console.log(rapidFood(1, 20));
