0c7e713a8a4c8aef617e41d9561c79fd230b7561
PatriciaKAssano <pkassano@gmail.com>
2022-12-11 13:00:11 -0300

    Revert "adding styles"
    This reverts commit 24201b33547ee8d0d83445df6e78471dec730bb9.

--- frontEnd_III/checkpoint_001/src/components/Card/index.jsx
+++ frontEnd_III/checkpoint_001/src/components/Card/index.jsx
@@ -1,9 +1,6 @@
-//import {useState} from 'react';
-import styles from './styles.css';
-
 function Card(props) {
     return (
-        <div id="card">
+        <div>
             <h3>{props.test.testName}</h3>
             <p>{props.test.testDate}</p>
             <p>{props.test.doctorName}</p>
